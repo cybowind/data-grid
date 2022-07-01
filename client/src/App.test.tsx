ae9ebe7204ca1e3de74bda548b05db3bab32b5e2
cybowind <90197435+cybowind@users.noreply.github.com>
2022-07-01 14:37:40 +0300

    Update App.test.tsx
    Removed typo in test description

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders viewer', () => {
   render(
     <Provider store={store}> 
       <App />
